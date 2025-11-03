dc463dc105963916256597e143d1cb55165b61e1
Ofuluefranklin07 <chikaofulue1@gmail.com>
2025-11-03 15:27:45 -0800

    update header text to My Todo App

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { HighlighterIcon } from "lucide-react";
 function Header() {
   return (
-    
     <header>
-        <div className="header-content">
-      <h1 className="text-[40px]">
-        <HighlighterIcon size={35}/>
-       My Keeper App
-      </h1>
-          </div>
+      <div className="header-content">
+        <h1 className="text-[40px]">
+          <HighlighterIcon size={35} />
+          My Todo App
+        </h1>
+      </div>
     </header>
-
   );
 }
 
